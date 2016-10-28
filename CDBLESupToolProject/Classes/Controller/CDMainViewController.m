6a7da49a268e278eb144dbbb9e14fa8edd0f1617
chendi <bbigcd@163.com>
2016-10-28 16:28:35 +0800

    set up tool

---
+++ CDBLESupToolProject/Classes/Controller/CDMainViewController.m
@@ -0,0 +1,37 @@
+//
+//  CDMainViewController.m
+//  CDBLESupToolProject
+//
+//  Created by bbigcd on 16/10/28.
+//  Copyright © 2016年 chendi. All rights reserved.
+//
+
+#import "CDMainViewController.h"
+
+@interface CDMainViewController ()
+
+@end
+
+@implementation CDMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
